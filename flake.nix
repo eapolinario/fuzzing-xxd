a25d58d5d53c10f54d79e69bd1173674164de457
Eduardo Apolinario <eapolinario@users.noreply.github.com>
2025-11-12 23:13:05 -0500

    feat(flake): add zzd package and include it in devShells for all systems
    Define zzd derivation for the zig implementation of xxd and add it to package sets and development shells across supported platforms.
    
    Signed-off-by: Eduardo Apolinario <eapolinario@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -5,12 +5,63 @@
 
   outputs = { self, nixpkgs }: let
     systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f (import nixpkgs { inherit system; }));
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs systems (system:
+        let pkgs = import nixpkgs { inherit system; };
+        in f { inherit system pkgs; });
+
+    mkZzd = pkgs: pkgs.stdenv.mkDerivation {
+      pname = "zzd";
+      version = "0.0.0-8a281ef";
+
+      src = pkgs.fetchFromGitHub {
+        owner = "eapolinario";
+        repo = "zzd";
+        rev = "8a281efb6a2fd10a4f01f419395a348176f7e556";
+        hash = "sha256-uS+Is/R7zvH6Riy3gEQdF7+zZwL0PHz4qDh2qaLqp24=";
+      };
+
+      nativeBuildInputs = [ pkgs.zig ];
+      dontConfigure = true;
+
+      buildPhase = ''
+        runHook preBuild
+        export ZIG_GLOBAL_CACHE_DIR="$TMPDIR/zig-cache"
+        export ZIG_LOCAL_CACHE_DIR="$TMPDIR/zig-cache"
+        zig build -Doptimize=ReleaseSafe
+        runHook postBuild
+      '';
+
+      installPhase = ''
+        runHook preInstall
+        export ZIG_GLOBAL_CACHE_DIR="$TMPDIR/zig-cache"
+        export ZIG_LOCAL_CACHE_DIR="$TMPDIR/zig-cache"
+        zig build install -Doptimize=ReleaseSafe --prefix "$out"
+        ln -s eapolinario_zzd "$out/bin/zzd"
+        runHook postInstall
+      '';
+
+      meta = with pkgs.lib; {
+        description = "Zig implementation of xxd";
+        homepage = "https://github.com/eapolinario/zzd";
+        license = licenses.unlicense;
+        platforms = platforms.unix;
+        mainProgram = "zzd";
+      };
+    };
   in {
-    devShells = forAllSystems (pkgs: {
+    packages = forAllSystems ({ pkgs, ... }: let
+      zzd = mkZzd pkgs;
+    in {
+      default = zzd;
+      inherit zzd;
+    });
+
+    devShells = forAllSystems ({ pkgs, system }: {
       default = pkgs.mkShell {
-        packages = with pkgs; [
-          radamsa
+        packages = [
+          pkgs.radamsa
+          self.packages.${system}.zzd
         ];
       };
     });
